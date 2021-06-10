7fef7cce218d8c4bcef0a749424dbd2c720eee61
Gaggo007 <gavriel.dinepi@gmail.com>
2021-06-10 22:59:24 +0200

    Pagina Artista migliorata e aggiunte al database

--- src/main/java/it/uniroma3/siw/museo/avvio/InizializzaDatabase.java
+++ src/main/java/it/uniroma3/siw/museo/avvio/InizializzaDatabase.java
@@ -100,6 +100,7 @@ public class InizializzaDatabase implements ApplicationListener<ContextRefreshed
 		a1.setDataMorte(LocalDate.of(1944, 12, 13));
 		a1.setLuogoMorte("Neuilly-sur-Seine");
 		a1.setNazione("Russia");
+		a1.setLinkFoto("Vassily-Kandinsky.jpeg");
 		as.inserisci(a1);
 		
 		Opera o1= new Opera();
@@ -114,6 +115,59 @@ public class InizializzaDatabase implements ApplicationListener<ContextRefreshed
 				+ "L'immagine è caratterizzata da diverse macchie colorate: alcune grosse velature espanse e trasparenti, situate in particolare nella parte alta del foglio, sembrano introdurre nella superficie bianca un senso di profondità fluttuante.");
 		o1.setLinkFoto("PrimoAcquarello.jpg");
 		os.inserisci(o1);
+		
+		Opera o2=new Opera();
+		o2.setArtista(a1);
+		o2.setTitolo("Composizione VIII");
+		o2.setCollezione(co1);
+		o2.setAnno(1923);
+		o2.setDescrizione("L'opera rappresenta forme geometriche elementari (cerchi, triangoli, quadrati, linee...), "
+				+ "disposte in maniera apparentemente casuale. Cerchi e linee rette sono le forme dominanti della composizione "
+				+ "e contribuiscono a creare una struttura geometrica rigida, rafforzata anche da colori opachi utilizzati per lo sfondo. All'interno del "
+				+ "quadro è possibile riconoscere anche trapezi, cunei e linee curve. Particolare rilievo è dato al cerchio viola, nero e rosso in alto a sinistra e al grande cuneo che taglia verticalmente il centro del dipinto.");
+		o2.setLinkFoto("Composizione VIII.jpg");
+		os.inserisci(o2);
+		
+		Artista a2=new Artista();
+		a2.setNome("Piet");
+		a2.setCognome("Mondrian");
+		a2.setDataNascita(LocalDate.of(1872,3,7));
+		a2.setLuogoNascita("Amersfoort");
+		a2.setDataMorte(LocalDate.of(1944, 2, 1));
+		a2.setLuogoMorte("New York");
+		a2.setNazione("Olanda");
+		a2.setLinkFoto("PietMondrian.jpg");
+		as.inserisci(a2);
+		
+		Opera o3=new Opera();
+		o3.setArtista(a2);
+		o3.setTitolo("Albero Rosso");
+		o3.setCollezione(co1);
+		o3.setAnno(1909);
+		o3.setDescrizione("Red Tree (L'albero rosso) è un quadro dipinto nel 1908/1909 da Piet Mondrian. "
+				+ "È una rappresentazione artistica di un albero (un melo) in cui l'artista non si preoccupa del tronco o dei "
+				+ "rami, bensì della loro forma e dei loro rapporti. Un segno deciso e di vario spessore evidenzia la struttura e cerca di impregnare nella composizione i rapporti che scaturiscono dai rami. I colori, prevalentemente rosso e blu, favoriscono tale semplificazione.\r\n"
+				+ "\r\n"+ "Attualmente è conservato nel museo municipale dell'Aia.\r\n"+ "\r\n"
+				+ "Nell'opera il tronco nodoso e ruvido è chiazzato da striature rosse che, sui rami nudi, "
+				+ "paiono fiammelle guizzanti. L'albero pare prima adagiarsi dolcemente a destra, assecondando linee a onde correnti; poi protende le punte di nuovo verso l'alto; a sinistra, invece, i rami sembrano accelerare la loro caduta, quasi pronti a immergersi nella terra.");
+		o3.setLinkFoto("AlberoRosso.jpg");
+		os.inserisci(o3);
+		
+		Opera o4=new Opera();
+		o4.setArtista(a2);
+		o4.setTitolo("Broadway Boogie-Woogie");
+		o4.setCollezione(co1);
+		o4.setAnno(1942);
+		o4.setDescrizione("Il quadro è composto da una quantità di quadrati di colore luminoso e brillante, che sembrano "
+				+ "quasi luccicare come un mosaico, attirando l'attenzione del fruitore.\r\n"+ "\r\n"+ "In questo quadro "
+				+ "spariscono i rigidi reticoli neri, gli stessi rettangoli dipinti nei quadri precedenti, non sono più "
+				+ "bordati da una linea nera: ora sono accostati uno all'altro. Tutto ciò ha lo scopo di riprodurre il frenetico ritmo del ballo del Boogie-Woogie. Quest'opera trasmette un effetto di vitalità, suggerendo l'atmosfera della città di Broadway, riprendendo il reticolato delle sue strade e lo sfrecciare in esse dei taxi gialli.\r\n"
+				+ "\r\n"+ "In queste opere finali, le forme hanno usurpato il ruolo delle linee, aprendo una nuova porta per lo sviluppo di Mondrian come astrattista. I quadri della serie \"Boogie-Woogie\" rappresentano il più radicale sviluppo dal momento del suo abbandono dell'arte rappresentativa avvenuto nel 1913. Mentre le opere degli anni '20 e '30 tendevano ad avere un'austerità quasi scientifica in esse, "
+				+ "queste sono quadri luminosi, vivaci e riflettono la musica ottimista e allegra che li ispirò e la città nella quale vennero prodotti.");
+		o4.setLinkFoto("Broadway_Boogie_Woogie.jpg");
+		os.inserisci(o4);
+		
+		
 	}
 
 }
